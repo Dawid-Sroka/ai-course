478b5635335df04675f5b420bd7cf575390e00d7
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-03-08 00:44:28 +0100

    Nauczyłem się liczyć opt_dist dla flipped rows

--- zad5.py
+++ zad5.py
@@ -14,60 +14,78 @@ input_lines = input_file.readlines()
 x_dim, y_dim = [int(d) for d in input_lines[0].split()]
 D = x_dim + y_dim
 r = x_dim / D
+m = [[0 for x in range(y_dim)] for y in range(x_dim)]
+
 arr = [int(n) for n in input_lines[1:]]
 x_arr = [int(n) for n in input_lines[1:x_dim+1]]
 y_arr = [int(n) for n in input_lines[x_dim+1:]]
 print(x_dim)
 print(y_dim)
 
-x_present = [0] * x_dim
-x_position = [-1] * x_dim
-y_present = [0] * y_dim
-y_position = [-1] * y_dim
+x_cnt = [0] * x_dim
+y_cnt = [0] * y_dim
+
 present = [0] * D
-position = [0] * D
 
 # unfinished = set(present)
 unfinished = set(range(D))
 print(unfinished)
 
-def can_add_in_col(row, col):
-    if x_position[row] <= col < x_position[row] + x_present[row]:
-        print("no")
-        return False
-    elif y_present[col] == y_arr[col]:
-        return False
-    elif row == y_position[col] - 1:
-        return True
-    elif row == y_position[col] + y_present[col]:
-        return True
-    elif y_present[col] == 0:
-        return True
+def opt_dist(input_sequence: list[int], D: int) -> None:
+    seq_len = len(input_sequence)
+    aux_prefix = [0] * D
+    working_seq = aux_prefix + input_sequence
+
+    agg_seq = [0] * (D + seq_len)
+    max_agg = 0
+
+    for i in range(D, seq_len + D):
+        agg_seq[i] = agg_seq[i-1] + working_seq[i] - working_seq[i-D]
+        if(agg_seq[i] > max_agg):
+            max_agg = agg_seq[i]
+
+    count_ones = sum(working_seq)
+    bits_to_unset = count_ones - max_agg
+    bits_to_set = D - max_agg
+    answer = bits_to_unset + bits_to_set
+    return answer
+
+def flip(matrix, row, col):
+    result = matrix
+    if matrix[row][col] == 0:
+        result[row][col] = 1
     else:
-        return False
+        result[row][col] = 0
+    return result
 
-# assumes 
-def gain_row(row, col):
-
-
-while len(unfinished) > 0:
-    victim = uniform(0,1)
-    victim = sample(sorted(unfinished),1)
-    print(victim)
-    if victim[0] < x_dim:
-        # row
-        print("row")
-        if x_present[row] == 0:
-            can_add_in_col
+def flip_in_row(matrix, row, col):
+    result = matrix[row].copy()
+    if matrix[row][col] == 0:
+        result[col] = 1
+    else:
+        result[col] = 0
+    return result
+    
+def main():
+    # while len(unfinished) > 0:
+    for i in range(1):
+        # victim = uniform(0,1)
+        victim = sample(sorted(unfinished),1)[0]
+        print(victim)
+        victim = 2
+        if victim < x_dim:
+            row = victim
+            print("row")
+            for col in range(y_dim):
+                min_dist = x_dim
+                flipped = flip_in_row(m, row, col)
+                print(flipped)
+                candidate = opt_dist(flipped, x_arr[row])
+                print(candidate)
+                min_dist = min(min_dist, candidate)
+            print(min_dist)
         else:
+            victim = victim - x_dim
+            break
 
-        for col in range(y_dim):
-            if col == x_position[row] - 1 :
-            if can_add_in_col(victim, i):
-
-        break
-    else:
-        victim = victim - x_dim
-        pass
-
-
+main()
