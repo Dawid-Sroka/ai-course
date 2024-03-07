74f00b1f294f1b66af76486c2110c94740489d39
Dawid-Sroka <94313700+Dawid-Sroka@users.noreply.github.com>
2024-03-07 13:32:14 +0100

    Clean unused code, delete debug prints

--- zad2.py
+++ zad2.py
@@ -7,18 +7,11 @@ input_lines = input_file.readlines()
 
 def print_split(word, word_len, prev):
     split = []
-    prev_len = len(prev)
-    # for i in range(word_len, 0,-1):
     i = word_len
     while i > 0:
         split.insert(0,word[prev[i]:i])
         i = prev[i]
-
-    # for i in range(word_len, 0,-1):
-    #     split.insert(0,word[prev[i]:i])
-    # print(split)
     splitted = " ".join(split) + '\n'
-    # print(splitted)
     output_file.write(splitted)
 
 def opt_split(word, dict):
@@ -26,24 +19,19 @@ def opt_split(word, dict):
     dp = [0] * (word_len+1)
     prev = [0] * (word_len+1)
     for i in range(1,word_len+1):   # i is through dp
-        # print(dp)
         max_dp = 0
         for j in range(i):          # j is through word
-            candidate = word[j:i]
-            if candidate+'\n' in dict:
-                # print(candidate)
-                candidate_max = dp[j] + len(candidate)**2
+            candidate = word[j:i] + '\n'
+            if candidate in dict:
+                candidate_max = dp[j] + (i-j)**2
                 if candidate_max > max_dp:
                     max_dp = candidate_max
                     prev[i] = j
         dp[i] = max_dp
-    # print(dp[word_len])
     print_split(word, word_len, prev)
 
 with open("words_for_ai1.txt", 'r') as dict_file:
     words = dict_file.readlines()
-    # print(words[0:4])
     dict = set(words)
     for line in input_lines:
         opt_split(line.rstrip('\n'), dict)
-    # opt_split('warasowa', dict)
